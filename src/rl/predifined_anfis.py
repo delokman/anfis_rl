c9d63e10b17f17f154f3d1b0a061676d90573e37
Marius-Juston <Marius.juston@hotmail.fr>
2021-08-19 12:15:30 -0500

    chnaged function predifed anfis model

--- src/rl/predifined_anfis.py
+++ src/rl/predifined_anfis.py
@@ -4,31 +4,27 @@ from anfis.joint_membership_optimized import JointTrapMembershipV2
 from anfis.trainer import make_joint_anfis
 
 
-class Anfis:
-    def __init__(self):
-        super(Anfis, self).__init__()
+def predefined_anfis_model():
+    parameter_values = [
+        [1.40812349319458, -0.06754400581121445, -0.06204153597354889],
+        [5.350396156311035, 0.152223140001297, 1.2252693176269531],
+        [2.367072820663452, 0.004285290837287903, 1.275625467300415],
+        [0.5395, 0.525, 0.6303, 0.4556]
+    ]
 
-    def my_model(self):
-        parameter_values = [
-            [1.40812349319458, -0.06754400581121445, -0.06204153597354889],
-            [5.350396156311035, 0.152223140001297, 1.2252693176269531],
-            [2.367072820663452, 0.004285290837287903, 1.275625467300415],
-            [0.5395, 0.525, 0.6303, 0.4556]
-        ]
+    x_joint_definitons = [
+        ('distance', JointTrapMembershipV2(*parameter_values[0], constant_center=True)),
+        ('theta_far', JointTrapMembershipV2(*parameter_values[1], constant_center=True)),
+        ('theta_near', JointTrapMembershipV2(*parameter_values[2], constant_center=True)),
+    ]
 
-        x_joint_definitons = [
-            ('distance', JointTrapMembershipV2(*parameter_values[0], constant_center=True)),
-            ('theta_far', JointTrapMembershipV2(*parameter_values[1], constant_center=True)),
-            ('theta_near', JointTrapMembershipV2(*parameter_values[2], constant_center=True)),
-        ]
+    output_names = ['angular_velocity']
 
-        output_names = ['angular_velocity']
+    mambani = JointSymmetricTriangleMembership(*parameter_values[4], False,
+                                               x_joint_definitons[0][1].required_dtype())
 
-        mambani = JointSymmetricTriangleMembership(*parameter_values[4], False,
-                                                   x_joint_definitons[0][1].required_dtype())
+    rules_type = ConsequentLayerType.MAMDANI
 
-        rules_type = ConsequentLayerType.MAMDANI
+    model = make_joint_anfis(x_joint_definitons, output_names, rules_type=rules_type, mamdani_defs=mambani)
 
-        model = make_joint_anfis(x_joint_definitons, output_names, rules_type=rules_type, mamdani_defs=mambani)
-
-        return model
+    return model
