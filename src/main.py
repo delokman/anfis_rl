5c7f7783fd086117bcfc3b73b5f872a46054b01b
Marius-Juston <marius.juston@hotmail.fr>
2021-08-20 18:35:35 -0500

    implemented a runnable epoch Gazebo world system

--- src/main.py
+++ src/main.py
@@ -1,17 +1,23 @@
 #! /usr/bin/env python
+import datetime
+import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 import rospy
+import torch
 from std_srvs.srv import Empty
 from robot_localization.srv import SetPose
 
+from torch.utils.tensorboard import SummaryWriter
+
+from anfis.utils import plot_fuzzy_consequent, plot_fuzzy_membership_functions, plot_fuzzy_variables
 from jackal import Jackal
 from path import Path
 from rl.ddpg import DDPGAgent
 from rl.predifined_anfis import predefined_anfis_model
 from rl.utils import fuzzy_error, reward
-from test_course import test_course
+from test_course import test_course, test_course2
 
 
 def call_service(service_name, service_type, data):
@@ -30,26 +36,28 @@ def reset_world():
     rospy.sleep(2)
 
 
-if __name__ == '__main__':
-    rospy.init_node('anfis_rl')
+def plot_anfis_data(epoch, agent):
+    anfis = agent.actor
 
+    plot_fuzzy_consequent(summary, anfis, epoch)
+    plot_fuzzy_membership_functions(summary, anfis, epoch)
+    plot_fuzzy_variables(summary, anfis, epoch)
+
+
+def epoch(i, agent, path, summary):
+    print(f"EPOCH {i}")
     reset_world()
-
-    test_path = test_course()  ####testcoruse MUST start with 0,0 . Check this out
-    test_path.append([1000, 1000])
 
     rate = rospy.Rate(100)
 
     jackal = Jackal()
-    path = Path(test_path)
+    path = Path(path)
 
     jackal.wait_for_publisher()
 
     default_linear_velocity = 1.5
 
     jackal.linear_velocity = default_linear_velocity
-
-    agent = DDPGAgent(3, 1, predefined_anfis_model())
 
     distance_errors = []
 
@@ -65,6 +73,7 @@ if __name__ == '__main__':
             break
 
         path_errors = fuzzy_error(current_point, target_point, future_point, jackal)
+        distance_errors.append(path_errors[0])
         path_errors = np.array(path_errors)
 
         #   for ddpg model
@@ -84,21 +93,45 @@ if __name__ == '__main__':
         # rewards = -1000
         state = agent.curr_states
         agent.curr_states = path_errors
+
         agent.memory.push(state, control_law, rewards, path_errors, done)  # control_law after gain or before gain?
         if len(agent.memory) > batch_size:
             agent.update(batch_size)
 
-        print(control_law)
+        # print(control_law)
         jackal.pub_motion()
         rate.sleep()
 
     # plot
-    test_path = np.array(test_path)
+    test_path = np.array(path.path)
     robot_path = np.array(jackal.robot_path)
-    plt.plot(test_path[:-1, 0], test_path[:-1, 1])
-    plt.plot(robot_path[:, 0], robot_path[:, 1])
-    plt.show()
 
-    # distance error mean
-    plt.show()
-    # print(np.mean(dis_error))
+    fig, ax = plt.subplots()
+    ax.plot(test_path[:-1, 0], test_path[:-1, 1])
+    ax.plot(robot_path[:, 0], robot_path[:, 1])
+
+    dist_error = np.mean(distance_errors)
+    print(dist_error)
+
+    summary.add_figure("Gazebo/Plot", fig, global_step=i)
+    summary.add_scalar("Error/Dist Error", dist_error, global_step=i)
+    plot_anfis_data(i, agent)
+
+    torch.save(os.path.join(summary.get_logdir(), "checkpoints", f"{i}-{dist_error}.chkp"), agent.state_dict())
+
+
+if __name__ == '__main__':
+    rospy.init_node('anfis_rl')
+
+    test_path = test_course2()  ####testcoruse MUST start with 0,0 . Check this out
+    test_path.append([1000, 1000])
+
+    name = f'Gazebo RL {datetime.datetime.now().strftime("%Y-%m-%D-%H-%M-%s")}'
+
+    summary = SummaryWriter(f'/home/auvsl/python3_ws/src/anfis_rl/runs/{name}/')
+
+    agent = DDPGAgent(3, 1, predefined_anfis_model())
+    # agent.load_state_dict(torch.load('input'))
+
+    for i in range(1000):
+        epoch(i, agent, test_path, summary)
