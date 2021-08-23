06998f1742fa4e1090c8000522f8789f3c6190a6
Marius-Juston <marius.juston@hotmail.fr>
2021-08-23 17:44:59 -0500

    Set mamdani center to be static

--- src/rl/predifined_anfis.py
+++ src/rl/predifined_anfis.py
@@ -9,7 +9,8 @@ def predefined_anfis_model():
         [0, 1.40812349319458, -0.06754400581121445, -0.06204153597354889],
         [0, 5.350396156311035, 0.152223140001297, 1.2252693176269531],
         [0, 2.367072820663452, 0.004285290837287903, 1.275625467300415],
-        [0.5395, 0.525, 0.6303, 0.4556]
+        # [0.5395, 0.525, 0.6303, 0.4556]
+        [0, 0.525, 0.6303, 0.4556]
     ]
 
     x_joint_definitons = [
@@ -20,7 +21,9 @@ def predefined_anfis_model():
 
     output_names = ['angular_velocity']
 
-    mambani = JointSymmetricTriangleMembership(*parameter_values[3], False,
+    # mambani = JointSymmetricTriangleMembership(*parameter_values[3], False,
+    #                                            x_joint_definitons[0][1].required_dtype())
+    mambani = JointSymmetricTriangleMembership(*parameter_values[3], True,
                                                x_joint_definitons[0][1].required_dtype())
 
     rules_type = ConsequentLayerType.MAMDANI
