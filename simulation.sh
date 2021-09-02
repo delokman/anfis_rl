b3fe5f3305fd9d8a100eedea2d09b9ee62ea5c89
Marius-Juston <marius.juston@hotmail.fr>
2021-09-02 00:49:30 -0500

    Added ability to change the friction coeffiecitn for the simulation

---
+++ simulation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Setting URDF Extra to="
+export JACKAL_URDF_EXTRAS='/home/auvsl/catkin_ws/src/anfis_rl/urdf/friction.urdf'
+echo "Set JACKAL_URDF_EXTRAS=$JACKAL_URDF_EXTRAS"
+# exit gracefully by returning a status
+
+roslaunch anfis_rl simulation.launch
+exit 0
+
