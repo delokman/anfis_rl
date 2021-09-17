216d6fce221abd8da83b80cd9d760d00d0152b1d
Marius-Juston <Marius.juston@hotmail.fr>
2021-09-16 23:39:37 -0500

    Implmeneted joint 7 trapezoid membership function

--- src/anfis/joint_membership_optimized.py
+++ src/anfis/joint_membership_optimized.py
@@ -298,3 +298,176 @@ class JointSingleConstrainedEdgeMembership(JointMembership):
         left = 1 - right
 
         return torch.cat([left, right], dim=1)
+
+
+class Joint7TrapMembership(JointMembership):
+    def required_dtype(self):
+        return torch.float
+
+    def left_x(self):
+        return self.center - self.half_width()
+
+    def half_width(self):
+        return torch.abs(self.center_width) / 2 + torch.abs(self.side_width) + torch.abs(
+            self.super_side_width) + 3 / (torch.abs(self.slope - self.slope_constraint) + self.slope_constraint)
+
+    def right_x(self):
+        return self.center + self.half_width()
+
+    def __init__(self, center, slope, center_width, side_width, super_side_width, constant_center=False,
+                 min_slope=0.01):
+        super().__init__()
+        self.slope_constraint = torch.tensor(min_slope, dtype=self.required_dtype())
+
+        if constant_center:
+            self.center = torch.tensor(center, dtype=self.required_dtype(), requires_grad=False)
+        else:
+            self.register_parameter('center', _mk_param(center, dtype=self.required_dtype()))
+
+        self.register_parameter('slope', _mk_param(slope, dtype=self.required_dtype()))
+        self.register_parameter('center_width', _mk_param(center_width, dtype=self.required_dtype()))
+        self.register_parameter('side_width', _mk_param(side_width, dtype=self.required_dtype()))
+        self.register_parameter('super_side_width', _mk_param(super_side_width, dtype=self.required_dtype()))
+
+        self.one = torch.tensor(1, dtype=self.required_dtype())
+        self.n_one = torch.tensor(-1, dtype=self.required_dtype())
+        self.two = torch.tensor(2, dtype=self.required_dtype())
+
+        # FIXME Lol cheeky way to add backwards compatability
+        mf_definitions = OrderedDict()
+        mf_definitions["Left Edge"] = self
+        mf_definitions["Left"] = self
+        mf_definitions["Close Left"] = self
+        mf_definitions["Center"] = self
+        mf_definitions["Close Right"] = self
+        mf_definitions["Right"] = self
+        mf_definitions["Right Edge"] = self
+        self.mfdefs = mf_definitions
+
+    @property
+    def num_mfs(self):
+        return len(self.mfdefs)
+
+    def fuzzify(self, x):
+        output = self.forward(x)
+
+        return (
+            ('Left Edge', output[:, 0]),
+            ("Left", output[:, 1]),
+            ("Close Left", output[:, 2]),
+            ("Center", output[:, 3]),
+            ("Close Right", output[:, 4]),
+            ("Right", output[:, 5]),
+            ("Right Edge", output[:, 6])
+        )
+
+    def forward(self, x):
+        # IMPLEMENT TECHNIQUE SO THAT THE OUTPUT MATRIX IS FIST ALL ZEROS.
+        # When the area is 1, for the other membership functions it is 0
+
+        slope = torch.abs(self.slope - self.slope_constraint) + self.slope_constraint
+
+        slope_width = torch.reciprocal(slope)
+
+        center_width = torch.abs(self.center_width)
+        side_width = torch.abs(self.side_width)
+        super_side_width = torch.abs(self.super_side_width)
+
+        x = x - self.center
+
+        ones = torch.ones_like(x, requires_grad=False)
+        zeros = torch.zeros_like(x, requires_grad=False)
+
+        # CENTER
+
+        x_center = torch.abs(x)
+
+        center_div_2 = torch.div(center_width, 2)
+
+        flat_area = torch.less_equal(x_center, center_div_2)
+        slopped = torch.less_equal(x_center, center_div_2 + slope_width)
+        slopped = (~flat_area) & slopped
+        slopped_value = - slope * (x_center - center_div_2) + 1
+
+        center = torch.where(flat_area, ones, torch.where(slopped, slopped_value, zeros))
+
+        # CLOSE LEFT AND RIGHT
+
+        side_div_2 = torch.div(side_width, 2)
+        shift = center_div_2 + side_div_2 + slope_width
+
+        mul_slope = slope * side_div_2 + 1
+
+        # LEFT
+        x_left = torch.abs(x + shift)
+
+        flat_area = torch.less_equal(x_left, side_div_2)
+        slopped = torch.less_equal(x_left, side_div_2 + slope_width)
+        slopped = (~flat_area) & slopped
+        # slopped_value = - slope * (x_left - side_div_2) + 1
+        slopped_value = - slope * x_left + mul_slope
+        close_left = torch.where(flat_area, ones, torch.where(slopped, slopped_value, zeros))
+
+        # RIGHT
+        x_right = torch.abs(x - shift)
+
+        flat_area = torch.less_equal(x_right, side_div_2)
+        slopped = torch.less_equal(x_right, side_div_2 + slope_width)
+        slopped = ~flat_area & slopped
+        # slopped_value = - slope * (x_right - side_div_2) + 1
+        slopped_value = - slope * x_right + mul_slope
+        close_right = torch.where(flat_area, ones, torch.where(slopped, slopped_value, zeros))
+
+        # LEFT AND RIGHT
+
+        super_side_div_2 = torch.div(super_side_width, 2)
+        shift = center_div_2 + side_width + super_side_div_2 + 2 * slope_width
+        mul_slope = slope * super_side_div_2 + 1
+
+        # LEFT
+        x_left = torch.abs(x + shift)
+
+        flat_area = torch.less_equal(x_left, super_side_div_2)
+        slopped = torch.less_equal(x_left, super_side_div_2 + slope_width)
+        slopped = (~flat_area) & slopped
+        # slopped_value = - slope * (x_left - side_div_2) + 1
+        slopped_value = - slope * x_left + mul_slope
+        left = torch.where(flat_area, ones, torch.where(slopped, slopped_value, zeros))
+
+        # RIGHT
+        x_right = torch.abs(x - shift)
+
+        flat_area = torch.less_equal(x_right, super_side_div_2)
+        slopped = torch.less_equal(x_right, super_side_div_2 + slope_width)
+        slopped = ~flat_area & slopped
+        # slopped_value = - slope * (x_right - side_div_2) + 1
+        slopped_value = - slope * x_right + mul_slope
+        right = torch.where(flat_area, ones, torch.where(slopped, slopped_value, zeros))
+
+        # EDGES
+
+        shift = (center_div_2 + side_width + 3 * slope_width + super_side_width)
+
+        mul_slope = slope * slope_width
+
+        # LEFT EDGE
+        x_left_edge = -x - shift
+
+        flat_area = torch.greater(x_left_edge, 0)
+        slopped = torch.greater_equal(x_left_edge, -slope_width)
+        slopped = (~flat_area) & slopped
+        slopped_value = slope * x_left_edge + mul_slope
+
+        left_edge = torch.where(flat_area, ones, torch.where(slopped, slopped_value, zeros))
+
+        # RIGHT EDGE
+        x_right_edge = x - shift
+
+        flat_area = torch.greater(x_right_edge, 0)
+        slopped = torch.greater_equal(x_right_edge, -slope_width)
+        slopped = (~flat_area) & slopped
+        slopped_value = slope * x_right_edge + mul_slope
+
+        right_edge = torch.where(flat_area, ones, torch.where(slopped, slopped_value, zeros))
+
+        return torch.cat([left_edge, left, close_left, center, close_right, right, right_edge], dim=1)
