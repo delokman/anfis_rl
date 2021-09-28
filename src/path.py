c7b59962c474fd5ef4bd018ed9bf08d3e978fd08
Marius-Juston <marius.juston@hotmail.fr>
2021-09-28 16:15:32 -0500

    So that there are no issues with missing multiple path edges have a while loop to check for next segments

--- src/path.py
+++ src/path.py
@@ -32,45 +32,51 @@ class Path:
     def get_trajectory(self, robot, progress_bar=None):
         pos_x, pos_y = robot.get_pose()
 
-        current_point = np.array(self.path[self.path_count])
-        target = np.array(self.path[self.path_count + 1])
+        continue_search = True
 
-        if self.transform is not None:
-            current_point = self.transform @ np.append(current_point, 1)
-            target = self.transform @ np.append(target, 1)
+        while continue_search:
 
-            current_point = current_point[:-1]
-            target = target[:-1]
+            current_point = np.array(self.path[self.path_count])
+            target = np.array(self.path[self.path_count + 1])
 
-        A = np.array([[(current_point[1] - target[1]), (target[0] - current_point[0])],
-                      [(target[0] - current_point[0]), (target[1] - current_point[1])]])
-        b = np.array([[(target[0] * current_point[1] - current_point[0] * target[1])],
-                      [(pos_x * (target[0] - current_point[0]) + pos_y * (target[1] - current_point[1]))]])
-        proj = np.matmul(np.linalg.inv(A), b)
+            if self.transform is not None:
+                current_point = self.transform @ np.append(current_point, 1)
+                target = self.transform @ np.append(target, 1)
 
-        current_point = np.array([[current_point[0]], [current_point[1]]])
-        target = np.array([[target[0]], [target[1]]])
-        temp1 = proj - current_point  ####dot product
-        temp2 = target - current_point
-        proj_len = (temp1[0] * temp2[0] + temp1[1] * temp2[1]) / np.linalg.norm(target - current_point, 2) ** 2
+                current_point = current_point[:-1]
+                target = target[:-1]
 
-        if proj_len > 1:
-            self.path_count += 1
+            A = np.array([[(current_point[1] - target[1]), (target[0] - current_point[0])],
+                          [(target[0] - current_point[0]), (target[1] - current_point[1])]])
+            b = np.array([[(target[0] * current_point[1] - current_point[0] * target[1])],
+                          [(pos_x * (target[0] - current_point[0]) + pos_y * (target[1] - current_point[1]))]])
+            proj = np.matmul(np.linalg.inv(A), b)
 
-            if progress_bar is not None:
-                progress_bar.update()
+            current_point = np.array([[current_point[0]], [current_point[1]]])
+            target = np.array([[target[0]], [target[1]]])
+            temp1 = proj - current_point  ####dot product
+            temp2 = target - current_point
+            proj_len = (temp1[0] * temp2[0] + temp1[1] * temp2[1]) / np.linalg.norm(target - current_point, 2) ** 2
 
-        if self.path_count == self.path_length - 1:
-            self.stop = True
+            if proj_len > 1:
+                self.path_count += 1
 
-        if (self.path_count == (self.path_length - 2)) or (self.path_count == (self.path_length - 1)):
-            curr = np.array(self.path[self.path_count])
-            tar = np.array(self.path[self.path_count + 1])
-            future = np.array(self.path[self.path_count + 1])
-        else:
-            curr = np.array(self.path[self.path_count])
-            tar = np.array(self.path[self.path_count + 1])
-            future = np.array(self.path[self.path_count + 2])
+                if progress_bar is not None:
+                    progress_bar.update()
+            else:
+                continue_search = False
+
+            if self.path_count == self.path_length - 1:
+                self.stop = True
+
+            if (self.path_count == (self.path_length - 2)) or (self.path_count == (self.path_length - 1)):
+                curr = np.array(self.path[self.path_count])
+                tar = np.array(self.path[self.path_count + 1])
+                future = np.array(self.path[self.path_count + 1])
+            else:
+                curr = np.array(self.path[self.path_count])
+                tar = np.array(self.path[self.path_count + 1])
+                future = np.array(self.path[self.path_count + 2])
 
         if self.transform is not None:
             curr = self.transform @ np.append(curr, 1)
