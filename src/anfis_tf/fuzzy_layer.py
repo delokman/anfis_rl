eeaec9542b0c0c48f572976d940e8d02446556b2
Marius-Juston <marius.juston@hotmail.fr>
2021-11-09 16:18:25 -0600

    made number of lingusitic variblae a constant

--- src/anfis_tf/fuzzy_layer.py
+++ src/anfis_tf/fuzzy_layer.py
@@ -10,6 +10,7 @@ class JointFuzzifyLayer(tf.Module):
             self.input_functions = input_functions
 
         self.max_outputs = tf.constant(max(i.num_outputs for i in self.input_functions))
+        self.num_lingustic_variables = tf.constant(len(self.input_functions))
 
         for func in self.input_functions:
             func.pad_to(self.max_outputs)
@@ -22,7 +23,7 @@ class JointFuzzifyLayer(tf.Module):
 
         i0 = [i, output]
 
-        c = lambda i, _: tf.less(i, len(self.input_functions))
+        c = lambda i, _: tf.less(i, self.num_lingustic_variables)
 
         # FIXME make this eager executable
         def foo(i, output):
