208569d23074f6856afddc106ed918f124f7e041
Marius-Juston <marius.juston@hotmail.fr>
2021-11-15 15:21:55 -0600

    added roslaunch for jackal to run environment setter

---
+++ env_export.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Setting URDF Extra to="
+export JACKAL_URDF_EXTRAS='/home/auvsl/catkin_ws/src/anfis_rl/urdf/friction.urdf'
+echo "Set JACKAL_URDF_EXTRAS=$JACKAL_URDF_EXTRAS"
+# exit gracefully by returning a status
+
+exit 0
+
