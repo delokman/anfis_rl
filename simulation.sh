8531448a737a552d1c348a0f68ee64ffedce0bb0
Marius Juston <marius.juston@hotmail.fr>
2022-02-11 16:33:42 -0600

    made it so that it updated the non python3 repository and runs the correct simulation fi

--- simulation.sh
+++ simulation.sh
@@ -5,6 +5,11 @@ export JACKAL_URDF_EXTRAS=$HOME'/python3_ws/src/anfis_rl/urdf/friction.urdf'
 echo "Set JACKAL_URDF_EXTRAS=$JACKAL_URDF_EXTRAS"
 # exit gracefully by returning a status
 
+source $HOME/catkin_ws/devel/setup.bash
+
+roscd anfis_rl
+git pull
+
 roslaunch anfis_rl simulation.launch
 exit 0
 
