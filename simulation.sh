e9790ff9508213235e0fd5859c5adf3a16c53501
Marius-Juston <marius.juston@hotmail.fr>
2022-02-11 15:54:45 -0600

    made the simulation.sh less dependent on system

--- simulation.sh
+++ simulation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Setting URDF Extra to="
-export JACKAL_URDF_EXTRAS='/home/auvsl/catkin_ws/src/anfis_rl/urdf/friction.urdf'
+export JACKAL_URDF_EXTRAS=$HOME'/python3_ws/src/anfis_rl/urdf/friction.urdf'
 echo "Set JACKAL_URDF_EXTRAS=$JACKAL_URDF_EXTRAS"
 # exit gracefully by returning a status
 
