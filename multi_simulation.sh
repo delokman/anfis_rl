4e38315242071552931ca92bb195a65bbca13fbd
Marius-Juston <marius.juston@hotmail.fr>
2022-03-24 01:05:19 -0500

    added position paramete ability to toggle world creation or not and enable or diable GUI

--- multi_simulation.sh
+++ multi_simulation.sh
@@ -17,6 +17,6 @@ roscd anfis_rl
 cp $HOME'/python3_ws/src/anfis_rl/launch/multi_simulation.launch' $HOME'/catkin_ws/src/anfis_rl/launch/multi_simulation.launch'
 cp $HOME'/python3_ws/src/anfis_rl/multi_simulation.sh' $HOME'/catkin_ws/src/anfis_rl/multi_simulation.sh'
 
-echo "roslaunch anfis_rl multi_simulation.launch -p "$1" "ns:=$2""
-/usr/bin/python2 /opt/ros/melodic/bin/roslaunch $HOME'/catkin_ws/src/anfis_rl/launch/multi_simulation.launch' -p "$1" "ns:=$2"
+echo "roslaunch anfis_rl multi_simulation.launch -p "$1" "ns:=$2"  "createWorld:=$3" "x:=$4" "y:=$5" "gui:=$6""
+/usr/bin/python2 /opt/ros/melodic/bin/roslaunch $HOME'/catkin_ws/src/anfis_rl/launch/multi_simulation.launch' -p "$1" "createWorld:=$2" "ns:=$3" "x:=$4" "y:=$5" "gui:=$6"
 exit 0
