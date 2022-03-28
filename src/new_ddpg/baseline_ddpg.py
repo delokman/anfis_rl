fe514a66880ece3ec3a4e8fe8ba9ec43f3ca2d40
Marius-Juston <marius.juston@hotmail.fr>
2022-03-28 16:15:00 -0500

    added model weights plotting to tensorboard

--- src/new_ddpg/baseline_ddpg.py
+++ src/new_ddpg/baseline_ddpg.py
@@ -1,19 +1,21 @@
 import time
+import traceback
+from typing import Optional
 
 import numpy as np
 import torch
 from matplotlib import pyplot as plt
-from stable_baselines3 import TD3
 from stable_baselines3.common.callbacks import BaseCallback
-from stable_baselines3.common.logger import Logger
+from stable_baselines3.common.logger import Logger, TensorBoardOutputFormat
 from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.td3 import TD3
 
-from anfis.utils import plot_critic_weights
+from anfis.utils import plot_model_weights
 from gazebo_utils.test_course import test_course3
-from main import plot_anfis_data
+from main import plot_anfis_model_data
 from new_ddpg.jackal_gym import GazeboJackalEnv
 from new_ddpg.policy import TD3Policy
-from rl.utils import reward4
+from rl.utils import reward
 
 
 def create_env():
@@ -33,19 +35,31 @@ class TensorboardCallback(BaseCallback):
     def __init__(self, verbose=0):
         super(TensorboardCallback, self).__init__(verbose)
 
+        self._log_freq = 1000
+
         self.obs_name = ["Graphs/Target Error", "Graphs/Distance Error", "Graphs/Theta Lookahead", "Graphs/theta_far",
                          "Graphs/Theta Near"]
         self.act_name = ["Logs/Yaw Rate", "Logs/Velocity"]
 
-    def _on_rollout_start(self) -> None:
+        self.epoch_num = 0
+
+        self.tb_formatter: Optional[TensorBoardOutputFormat] = None
+
+    def _on_training_start(self) -> None:
         """
         A rollout is the collection of environment interaction
         using the current policy.
         This event is triggered before collecting new samples.
         """
-        # plot_critic_weights(summary, model.policy.critic, epoch)
-        # plot_critic_weights(summary, model.policy.critic_target, epoch)
-        # plot_anfis_data(summary, epoch, agent)
+        output_formats = self.logger.output_formats
+        # note: the failure case (not formatter found) is not handled here, should be done with try/except.
+        self.tb_formatter: TensorBoardOutputFormat = next(
+            formatter for formatter in output_formats if isinstance(formatter, TensorBoardOutputFormat))
+
+        plot_model_weights(self.tb_formatter.writer, model.policy.critic, -1)
+        # plot_model_weights(self.tb_formatter.writer, model.policy.critic_target, -1)
+        plot_anfis_model_data(self.tb_formatter.writer, -1, model.policy.actor)
+        # plot_anfis_model_data(self.tb_formatter.writer, -1, model.policy.actor_target)
 
     def _on_step(self) -> bool:
         """
@@ -68,7 +82,8 @@ class TensorboardCallback(BaseCallback):
         for act_name, act in zip(self.act_name, act[0]):
             self.logger.record(act_name, act, exclude=("stdout"))
 
-        self.logger.dump(self.num_timesteps)
+        if self.n_calls % self._log_freq == 0:
+            self.logger.dump(self.num_timesteps)
 
         return True
 
@@ -76,10 +91,12 @@ class TensorboardCallback(BaseCallback):
         """
         This event is triggered before updating the policy.
         """
-        # plot_critic_weights(summary, model.policy.critic, epoch)
-        # plot_critic_weights(summary, model.policy.critic_target, epoch)
-        # plot_anfis_data(summary, epoch, agent)
+        plot_model_weights(self.tb_formatter.writer, model.policy.critic, self.epoch_num)
+        # plot_model_weights(self.tb_formatter.writer, model.policy.critic_target, self.epoch_num)
+        plot_anfis_model_data(self.tb_formatter.writer, self.epoch_num, model.policy.actor)
+        # plot_anfis_model_data(self.tb_formatter.writer, self.epoch_num, model.policy.actor_target)
 
+        self.epoch_num += 1
 
 
 if __name__ == '__main__':
