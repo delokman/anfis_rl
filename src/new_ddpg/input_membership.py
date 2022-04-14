92e87880cdc7dc1e7048dc946c8090e28f3cf29e
Marius-Juston <marius.juston@hotmail.fr>
2022-04-14 18:17:40 -0500

    moved test function to seperate places

--- src/new_ddpg/input_membership.py
+++ src/new_ddpg/input_membership.py
@@ -4,11 +4,9 @@ from typing import List
 import numpy as np
 import torch
 from matplotlib import pyplot as plt
+from torch import profiler
 
 from anfis.joint_membership import _mk_param
-from vizualize.auto_grad_viz import make_dot
-
-torch.autograd.set_detect_anomaly(True)
 
 
 class JointMembership(ABC, torch.nn.Module):
@@ -66,7 +64,7 @@ class JointTrapMembership(JointMembership):
         return out
 
 
-if __name__ == '__main__':
+def test():
     weights = torch.log(torch.tensor([[1], [2], [3], [4.]], requires_grad=True, device='cuda'))
 
     c = torch.tensor(0.0, device=weights.device, requires_grad=True)
@@ -74,29 +72,95 @@ if __name__ == '__main__':
     with torch.no_grad():
         a = torch.cumsum(weights.exp(), dim=0)[-1] * 1.5
         a = a.item()
-        x = torch.linspace(c - a, c + a, steps=1000)
+        x = torch.linspace(c - a, c + a, steps=1)
         x = x.repeat(5, 1)
         x = x.to(c.device)
 
     trapezoid = JointTrapMembership(0, np.log(np.array([1, 2, 3, 4])))
     trapezoid.cuda()
+    funcs = {'forward': x}
+    trapezoid_opt = torch.jit.trace_module(trapezoid, funcs)
+
+    o = []
+    o2 = []
+    y = []
+
+    print("Starting")
+
+    for power in range(1, 65):
+        with torch.no_grad():
+            a = torch.cumsum(weights.exp(), dim=0)[-1] * 1.5
+            a = a.item()
+            x = torch.linspace(c - a, c + a, steps=power)
+            x = x.repeat(5, 1)
+            x = x.to(c.device)
+
+        with profiler.profile() as prof:
+            for _ in range(100):
+                trapezoid(x)
+        print(prof.key_averages().table(sort_by='cuda_time_total', row_limit=5))
+
+        diff1 = prof.key_averages().total_average().self_cpu_time_total
+        cuda1 = prof.key_averages().total_average().self_cuda_time_total
+
+        with profiler.profile() as prof:
+            for _ in range(100):
+                trapezoid_opt(x)
+        print(prof.key_averages().table(sort_by='cuda_time_total', row_limit=5))
+
+        diff2 = prof.key_averages().total_average().self_cpu_time_total
+        cuda2 = prof.key_averages().total_average().self_cuda_time_total
+
+        o.append((diff1, diff2))
+        o2.append((cuda1, cuda2))
+
+        y.append(power)
+    o = np.array(o)
+    plt.plot(y, o[:, 0], label='normal')
+    plt.plot(y, o[:, 1], label='optimal')
+    plt.legend()
+    plt.show()
+
+    o = np.array(o2)
+    plt.plot(y, o[:, 0], label='normal')
+    plt.plot(y, o[:, 1], label='optimal')
+    plt.legend()
+    plt.show()
+
+
+def plotted():
+    weights = np.array([1., 2, 3])
+    c = 0
+
+    with torch.no_grad():
+        a = np.cumsum(weights, axis=0)[-1] * 1.5
+        temp_c = c
+        x = torch.linspace(temp_c - a, temp_c + a, steps=1000)
+        x = x.reshape((-1, 1))
+
+    trapezoid = JointTrapMembership(c, np.log(weights), constant_center=False)
+    trapezoid.cuda()
+    x = x.cuda()
 
     Y = trapezoid(x)
+    crit = torch.nn.MSELoss()
 
-    loss = Y.sum()
+    zero = Y - 0.1
+    zero = zero.detach()
+
+    loss: torch.Tensor = crit(Y, zero)
     loss.backward()
 
-    dot = make_dot(loss, dict(weight=weights))
-    dot.view()
+    print(loss)
+    print(trapezoid.center.grad)
 
-    # trap = torch.jit.trace(trap, example_inputs=(x, c, weights))
-
-    # print(trap.code)
-    # print(trap.graph)
-    #
     Y = Y.cpu().detach()
     x = x.cpu().detach()
 
-    plt.plot(x.T, Y.T)
+    plt.plot(x, Y)
     plt.title('Pytorch Trapezoid Function')
     plt.show()
+
+
+if __name__ == '__main__':
+    plotted()
